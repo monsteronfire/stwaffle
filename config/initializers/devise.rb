91fdf80705b0ea0f976d6288e0bc0f0cbcbcf9d4
monsteronfire <monsteronfire@gmail.com>
2015-12-08 15:41:50 +0800

    fixes typo

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  cconfig.mailer_sender = 'monsteronfire@gmail.com'
+  config.mailer_sender = 'monsteronfire@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
