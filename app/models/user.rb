61f9c02308007d5893ca6128dc62733a0108722e
monsteronfire <monsteronfire@gmail.com>
2015-12-08 13:30:09 +0800

    adds config line for devise in production config

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :waffles
+  has_one :waffle
 end
